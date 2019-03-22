8ab552fd8a6d775047f75ce740323ac94c430a82
Chengdong <sugimura1996@hotmail.com>
2019-03-22 14:42:05 +0000

    added baseline

--- read_data_aug.py
+++ read_data_aug.py
@@ -11,17 +11,17 @@ import keras
 from tqdm import tqdm
 import glob
 import random
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 splits = ['a', 'b', 'c', 'view', 'illum']
-tps = ['ref', 'e1', 'e2', 'e3', 'e4', 'e5', 'h1', 'h2', 'h3', 'h4', 'h5', \
-       't1', 't2', 't3', 't4', 't5']
+tps = ['ref','e1','e2','e3','e4','e5','h1','h2','h3','h4','h5',\
+       't1','t2','t3','t4','t5']
 
 
 class DenoiseHPatches(keras.utils.Sequence):
     """Class for loading an HPatches sequence from a sequence folder"""
     itr = tps
-
-    def __init__(self, seqs, batch_size=32):
+    def __init__(self, seqs, batch_size = 32):
         self.all_paths = []
         self.batch_size = batch_size
         self.dim = (32, 32)
@@ -63,7 +63,7 @@ class DenoiseHPatches(keras.utils.Sequence):
         img_noise = np.empty((self.batch_size,) + self.dim + (self.n_channels,))
 
         for i in range(self.batch_size):
-            img, img_n = self.get_images(index * self.batch_size + i)
+            img, img_n = self.get_images(index*self.batch_size+i)
             img_clean[i] = np.expand_dims(img, -1)
             img_noise[i] = np.expand_dims(img_n, -1)
 
@@ -73,11 +73,9 @@ class DenoiseHPatches(keras.utils.Sequence):
         # 'Updates indexes after each epoch'
         random.shuffle(self.all_paths)
 
-
 class hpatches_sequence_folder:
     """Class for loading an HPatches sequence from a sequence folder"""
     itr = tps
-
     def __init__(self, base, noise=1):
         name = base.split('/')
         self.name = name[-1]
@@ -87,10 +85,11 @@ class hpatches_sequence_folder:
         else:
             noise_path = ''
         for t in self.itr:
-            im_path = os.path.join(base, t + noise_path + '.png')
-            im = cv2.imread(im_path, 0)
-            self.N = im.shape[0] / 32
+            im_path = os.path.join(base, t+noise_path+'.png')
+            im = cv2.imread(im_path,0)
+            self.N = im.shape[0]/32
             setattr(self, t, np.split(im, self.N))
+
 
 
 def generate_triplets(labels, num_triplets, batch_size):
@@ -101,7 +100,6 @@ def generate_triplets(labels, num_triplets, batch_size):
                 inds[ind] = []
             inds[ind].append(idx)
         return inds
-
     triplets = []
     indices = create_indices(np.asarray(labels))
     unique_labels = np.unique(np.asarray(labels))
@@ -140,6 +138,13 @@ class HPatches():
         self.test_fnames = test_fnames
         self.denoise_model = denoise_model
         self.use_clean = use_clean
+        self.imgaug= ImageDataGenerator(
+                # featurewise_center=True,
+                # featurewise_std_normalization=True,
+                rotation_range=20,
+                width_shift_range=0.2,
+                height_shift_range=0.2,
+                horizontal_flip=True)
 
     def set_denoise_model(self, denoise_model):
         self.denoise_model = denoise_model
@@ -150,16 +155,17 @@ class HPatches():
             batch = patches[i * batch_size:(i + 1) * batch_size]
             batch = np.expand_dims(batch, -1)
             batch = np.clip(self.denoise_model.predict(batch).astype(int),
-                            0, 255).astype(np.uint8)[:, :, :, 0]
-            patches[i * batch_size:(i + 1) * batch_size] = batch
-        batch = patches[i * batch_size:]
+                                        0, 255).astype(np.uint8)[:,:,:,0]
+            patches[i*batch_size:(i+1)*batch_size] = batch
+        batch = patches[i*batch_size:]
         batch = np.expand_dims(batch, -1)
         batch = np.clip(self.denoise_model.predict(batch).astype(int),
-                        0, 255).astype(np.uint8)[:, :, :, 0]
-        patches[i * batch_size:] = batch
+                        0, 255).astype(np.uint8)[:,:,:,0]
+        patches[i*batch_size:] = batch
         return patches
 
-    def read_image_file(self, data_dir, train=1):
+    def read_image_file(self, data_dir, train = 1):
+
         """Return a Tensor containing the patches
         """
         if self.denoise_model and not self.use_clean:
@@ -179,22 +185,30 @@ class HPatches():
             list_dirs = self.test_fnames
 
         for directory in tqdm(hpatches_sequences, file=sys.stdout):
-            if (directory in list_dirs):
-                for tp in tps:
-                    if self.use_clean:
-                        sequence_path = os.path.join(data_dir, directory, tp) + '.png'
-                    else:
-                        sequence_path = os.path.join(data_dir, directory, tp) + '_noise.png'
-                    image = cv2.imread(sequence_path, 0)
-                    h, w = image.shape
-                    n_patches = int(h / w)
-                    for i in range(n_patches):
-                        patch = image[i * (w): (i + 1) * (w), 0:w]
-                        patch = cv2.resize(patch, (32, 32))
-                        patch = np.array(patch, dtype=np.uint8)
-                        patches.append(patch)
-                        labels.append(i + counter)
-                counter += n_patches
+           if (directory in list_dirs):
+            for tp in tps:
+                if self.use_clean:
+                    sequence_path = os.path.join(data_dir, directory, tp)+'.png'
+                else:
+                    sequence_path = os.path.join(data_dir, directory, tp)+'_noise.png'
+                image = cv2.imread(sequence_path, 0)
+                h, w = image.shape
+                n_patches = int(h / w)
+                n_aug=3
+                for i in range(n_patches):
+                    patch = image[i * (w): (i + 1) * (w), 0:w]
+                    patch = cv2.resize(patch, (32, 32))
+                    patch = np.array(patch, dtype=np.uint8)
+                    patches.append(patch)
+                    labels.append(i +n_aug*i+ counter)
+                    for j in range(0,n_aug):
+                        params = self.imgaug.get_random_transform(patch.shape)
+                        patch_aug = self.imgaug.apply_transform(self.imgaug.standardize(patch), params)
+                        patches.append(patch_aug)
+                        labels.append(i+ j + counter)
+
+
+            counter += n_patches
 
         patches = np.array(patches, dtype=np.uint8)
         if self.denoise_model and not self.use_clean:
@@ -205,7 +219,7 @@ class HPatches():
 
 class DataGeneratorDesc(keras.utils.Sequence):
     # 'Generates data for Keras'
-    def __init__(self, data, labels, num_triplets=1000000, batch_size=50, dim=(32, 32), n_channels=1, shuffle=True):
+    def __init__(self, data, labels, num_triplets = 1000000, batch_size=50, dim=(32,32), n_channels=1, shuffle=True):
         # 'Initialization'
         self.transform = None
         self.out_triplets = True
@@ -249,7 +263,7 @@ class DataGeneratorDesc(keras.utils.Sequence):
         if self.out_triplets:
             img_n = np.empty((self.batch_size,) + self.dim + (self.n_channels,))
         for i in range(self.batch_size):
-            t = self.triplets[self.batch_size * index + i]
+            t = self.triplets[self.batch_size*index + i]
             img_a_t, img_p_t, img_n_t = self.get_image(t)
             img_a[i] = img_a_t
             img_p[i] = img_p_t
@@ -261,3 +275,6 @@ class DataGeneratorDesc(keras.utils.Sequence):
     def on_epoch_end(self):
         # 'Updates indexes after each epoch'
         self.triplets = generate_triplets(self.labels, self.num_triplets, 32)
+
+
+
